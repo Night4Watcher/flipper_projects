5582103fc09c58dd3fc0b2112d80ae9007661d11
Night4Watcher <andoni.hilario.lazaro@gmail.com>
2025-12-10 16:53:21 +0100

    finalizado

--- flipper/ruleta_rusa.py
+++ flipper/ruleta_rusa.py
@@ -1,6 +1,20 @@
 import random
 import os
+from time import sleep
 
-print("prueba")
+def main():
+    numero_final = random.randint(0, 10)
+    segundos = 9
+    os.system("clear")
+    for c in range(segundos):
+        sleep(1)
+        if c == numero_final:
+            print("Bye Bye")
+            os.system("shutdown now")
+        print(c)
+    print("Time Out....")
+    print("You survived for now...")
 
+if (__name__ == "__main__"):
+    main()
 
